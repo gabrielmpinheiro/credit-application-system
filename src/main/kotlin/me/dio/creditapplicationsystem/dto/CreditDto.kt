d88af9738d00bfeb923bcef3e52a8a8bf0188c59
Gabriel Pinheiro <gabrielmpinheiro2@gmail.com>
2023-10-02 18:31:02 -0300

    Correção de data no CreditDto

--- src/main/kotlin/me/dio/creditapplicationsystem/dto/CreditDto.kt
+++ src/main/kotlin/me/dio/creditapplicationsystem/dto/CreditDto.kt
@@ -9,13 +9,13 @@ import java.time.LocalDate
 
 data class CreditDto (
     val creditValue: BigDecimal,
-    @field:Future() val dayFirstInstallment: LocalDate,
+    @field:Future() val dayFirstOfInstallment: LocalDate,
     val numberOfInstallments: Int,
     @field:NotNull(message = "Invalid INput") val customerId: Long
 ) {
     fun toEntity(): Credit = Credit(
         creditValue = this.creditValue,
-        dayFirstInstallment = this.dayFirstInstallment,
+        dayFirstInstallment = this.dayFirstOfInstallment,
         numberOfInstallments = this.numberOfInstallments,
         customer = Customer(id = this.customerId)
     )
